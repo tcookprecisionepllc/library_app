bf72e71692f38812c8956e649ed7fa7a3c12ab3a
Tony Cook <tcook@precisionepllc.com>
2019-07-04 14:14:18 -0500

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,7 @@
+{
+    'name': 'Library Management',
+    'description': 'Manage library book calogue and lending.',
+    'author': 'Daniel Reis',
+    'depends': ['base'],
+    'application': True,
+}
