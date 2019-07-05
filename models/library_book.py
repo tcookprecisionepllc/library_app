021394dcc8350b00c90d32c62b2859ee9423029f
Tony Cook <tcook@precisionepllc.com>
2019-07-04 21:44:53 -0500

    Adding business logic

--- models/library_book.py
+++ models/library_book.py
@@ -1,6 +1,28 @@
-from odoo import fields, models
+from odoo import api, fields, models
+from odoo.exceptions import Warning
 
 class Book(models.Model):
+
+    @api.multi
+    def _check_isbn(self):
+        self.ensure_one()
+        digits = [int(x) for x in self.isbn if x.isdigit()]
+        if len(digits) == 13:
+            ponderations = [1, 3] * 6
+            terms = [a * b for a, b in zip(digits[:12], ponderations)]
+            remain = sum(terms) % 10
+            check = 10 - remain if remain != 0 else 0
+            return digits[-1] == check
+
+    @api.multi
+    def button_check_isbn(self):
+        for book in self:
+            if not book.isbn:
+                raise Warning('Please provide an ISBN for %s' % book.name)
+            if book.isbn and not book._check_isbn():
+                raise Warning('%s is in invalid ISBN' % book.isbn)
+        return True
+
     _name = 'library.book'
     _description = 'Book'
     name = fields.Char('Title', required=True)
