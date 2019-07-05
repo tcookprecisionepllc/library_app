cdc5cec663bae13efccf94abc2f4f62d5a25cae0
Tony Cook <tcook@precisionepllc.com>
2019-07-05 13:03:35 -0500

    Adding additional book fields

--- models/library_book.py
+++ models/library_book.py
@@ -25,10 +25,27 @@ class Book(models.Model):
 
     _name = 'library.book'
     _description = 'Book'
+    _order = 'name, date_published desc'
     name = fields.Char('Title', required=True)
     isbn = fields.Char('ISBN')
+    book_type = fields.Selection(
+        [('paper', 'Paperback'),
+        ('hard', 'Hardback'),
+        ('electronic', 'Electronic'),
+        ('other', 'Other')
+        ],
+        'Type')
+    notes = fields.Text('Internal Notes')
+    descr = fields.Html('Description')
+    copies = fields.Integer(default=1)
+    avg_rating = fields.Float('Average Rating', (3, 2))
+    price = fields.Monetary('Price', 'currency_id')
+    currency_id = fields.Many2one('res.currency')
     active = fields.Boolean('Active?', default=True)
     date_published = fields.Date()
+    last_borrow_date = fields.Datetime(
+        'Last Borrowed On',
+        default=lambda self: fields.Datetime.now())
     image = fields.Binary('Cover')
     publisher_id = fields.Many2one('res.partner', string='Publisher')
     author_ids = fields.Many2many('res.partner', string='Authors')
