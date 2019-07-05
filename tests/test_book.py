eaceb8b049d18b49314f2bf08f8ddff01a823697
Tony Cook <tcook@precisionepllc.com>
2019-07-04 19:51:48 -0500

    Adding Book model

--- tests/test_book.py
+++ tests/test_book.py
@@ -7,8 +7,7 @@ class TestBook(TransactionCase):
         self.Book = self.env['library.book']
         self.book_ode = self.Book.create({
             'name': 'Odoo Development Essentials',
-            'isbn': '879-1-78439-279-6'
-        })
+            'isbn': '879-1-78439-279-6'})
         return result
 
     def test_create(self):
@@ -26,6 +25,5 @@ class TestBook(TransactionCase):
         self.Book = self.env['library.book']
         self.book_ode = self.Book.create({
             'name': 'Odoo Development Essentials'
-            'isbn': '879-1-78439-279-6'
-        })
+            'isbn': '879-1-78439-279-6'})
         return result
